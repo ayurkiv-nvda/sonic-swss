85431ea317e9b7bcfd8e95ba7246e3412c09a9c2
Andriy Yurkiv <ayurkiv@nvidia.com>
2025-06-12 18:13:19 +0300

    CoPP neighbor miss trap tests alignment
    Signed-off-by: Andriy Yurkiv <ayurkiv@nvidia.com>

--- tests/mock_tests/copporch_ut.cpp
+++ tests/mock_tests/copporch_ut.cpp
@@ -299,7 +299,7 @@ namespace copporch_test
 
         const auto &supportedTrapIds = Portal::CoppOrchInternal::getSupportedTrapIds(coppOrch.get());
         EXPECT_TRUE(supportedTrapIds.find(SAI_HOSTIF_TRAP_TYPE_IP2ME) != supportedTrapIds.end());
-        EXPECT_TRUE(supportedTrapIds.find(SAI_HOSTIF_TRAP_TYPE_NEIGHBOR_MISS) == supportedTrapIds.end());
+        EXPECT_TRUE(supportedTrapIds.find(SAI_HOSTIF_TRAP_TYPE_NEIGHBOR_MISS) != supportedTrapIds.end());
     }
 
     TEST_F(CoppOrchTest, TrapGroup_AddRemove)
@@ -500,8 +500,8 @@ namespace copporch_test
             const auto &tidList = Portal::CoppOrchInternal::getTrapIdsFromTrapGroup(coppOrch.get(), tgOid);
             const auto &tidSet = std::set<sai_hostif_trap_type_t>(tidList.begin(), tidList.end());
 
-            // Verify that neighbor_miss is not installed
-            EXPECT_TRUE(tidSet.find(SAI_HOSTIF_TRAP_TYPE_NEIGHBOR_MISS) == tidSet.end());
+            // Verify that neighbor_miss is installed
+            EXPECT_TRUE(tidSet.find(SAI_HOSTIF_TRAP_TYPE_NEIGHBOR_MISS) != tidSet.end());
 
             // Verify that bgp and bgpv6 are installed
             EXPECT_TRUE(trapIDSet == tidSet);
